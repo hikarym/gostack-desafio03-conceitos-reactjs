87281a6626f0a413a3783ec16ae7c44e38af0ff1
Mariela Fernandez <maratausinchi@gmail.com>
2020-06-25 02:56:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<h3 align="center">
+  Challenge 03: ReactJS Concepts
+</h3>
+
+<blockquote align="center">“Don't wait to plant, only have patience to harvest”!</blockquote>
+
+<p align="center">
+
+  <a href="https://github.com/Rocketseat/bootcamp-gostack-desafios">
+    GoStack's Challengues made during Rockeseat Bootcamp
+  </a>
+
+</p>
+
+<p align="center">
+  <a href="#rocket-about-the-challenge">About the Challengue</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#tests-tests-specification">Tests</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licence">Licence</a>
+</p>
+
+## :rocket: About the Challengue
+
+This is an application ReactJS to storage repositories of your portfolio, that will allow you to list, update, and delete repositories, and besides that, the repositories can also receive likes.
+
+You will can learn concepts about NodeJS.
+
+### Initialize and Execute the APP
+
+$ yarn 
+$ yarn start
+
---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {       
+      title: `Ecoleta NLW ${Date.now()}`, 
+      url: "https://github.com/hikarym/nlw-ecoleta",
+      techs: "NodeJS, ReactJS, ReactNative",
+      likes: 0       
+    }); 
+
+    const repository = response.data;
+
+    setRepositories([ ...repositories, repository ]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`);
+
+    const repositoriesUpdated = repositories.filter(repository => repository.id !== id);
+
+    setRepositories(repositoriesUpdated);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => 
+        <li key={repository.id}>          
+          {repository.title}
+          <button onClick={() => {
+              return handleRemoveRepository(repository.id);
+            }}>
+            Remover
+          </button>
+        </li>
+        )}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
